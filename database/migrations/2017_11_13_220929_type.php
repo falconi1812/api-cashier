9d230c9c7f1eb470154dbe5def867ea7a2afa88b
Christian Falcon <thefalcon1812@hotmail.com>
2017-11-18 16:02:14 +0000

    :heavy_plus_sign: Add migrations

--- database/migrations/2017_11_13_220929_type.php
+++ database/migrations/2017_11_13_220929_type.php
@@ -15,7 +15,6 @@ class Type extends Migration
     {
         Schema::create('type', function (Blueprint $table) {
             $table->increments('id');
-//            $table->primary('id');
             $table->string('name');
             $table->timestamps();
         });
