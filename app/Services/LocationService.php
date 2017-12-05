342ad4fe64cd1f435e5fb517efabb45baea5b85d
Christian Falcon <thefalcon1812@hotmail.com>
2017-12-05 20:14:47 +0000

    :heavy_plus_sign: :flag-de: Add repositories pattern and endpoint parser

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -18,43 +18,47 @@ class LocationService extends Service
     /**
      * @SWG\Definition(
      * 		definition="LocationByCode",
-     *      @SWG\Property(property="id", type="string"),
-     *      @SWG\Property(property="code", type="string"),
-     *      @SWG\Property(property="players", type="string"),
-     *      @SWG\Property(property="hour_end", type="string"),
-     *      @SWG\Property(property="hour_start", type="string"),
-     *      @SWG\Property(property="type_id", type="string"),
-     *      @SWG\Property(property="terrain_id", type="string"),
-     *      @SWG\Property(property="created_at", type="string"),
-     *      @SWG\Property(property="updated_at", type="string"),
-     * 		  @SWG\Property(property="products", type="array", @SWG\Items(
-     *          type="object",
-     *              @SWG\Property(property="id", type="string"),
-     *              @SWG\Property(property="product_id", type="string"),
-     *              @SWG\Property(property="location_id", type="string"),
-     *              @SWG\Property(property="products_in_list", type="string"),
-     *              @SWG\Property(property="products_in_payment", type="string"),
-     *              @SWG\Property(property="total_in_list", type="string"),
-     *              @SWG\Property(property="total_in_payment", type="string"),
-     *              @SWG\Property(property="total_payed", type="string"),
-     *              @SWG\Property(property="created_at", type="string"),
-     *              @SWG\Property(property="updated_at", type="string"),
-     *      )
+     *    @SWG\Property(property="products", type="object",
+     *          @SWG\Property(property="id", type="string"),
+     *          @SWG\Property(property="product_id", type="string"),
+     *          @SWG\Property(property="location_id", type="string"),
+     *          @SWG\Property(property="products_in_list", type="string"),
+     *          @SWG\Property(property="products_in_payment", type="string"),
+     *          @SWG\Property(property="total_in_list", type="string"),
+     *          @SWG\Property(property="total_in_payment", type="string"),
+     *          @SWG\Property(property="total_payed", type="string"),
+     *          @SWG\Property(property="created_at", type="string"),
+     *          @SWG\Property(property="updated_at", type="string"),
      *    ),
-     *    @SWG\Property(property="client", type="array", @SWG\Items(
-     *          type="object",
-     *              @SWG\Property(property="id", type="string"),
-     *              @SWG\Property(property="name", type="string"),
-     *              @SWG\Property(property="last_name", type="string"),
-     *              @SWG\Property(property="email", type="string"),
-     *              @SWG\Property(property="phone", type="string"),
-     *      )
+     *    @SWG\Property(property="location", type="object",
+     *          @SWG\Property(property="id", type="string"),
+     *          @SWG\Property(property="code", type="string"),
+     *          @SWG\Property(property="players", type="string"),
+     *          @SWG\Property(property="hour_end", type="string"),
+     *          @SWG\Property(property="hour_start", type="string"),
+     *          @SWG\Property(property="type_id", type="string"),
+     *          @SWG\Property(property="terrain_id", type="string"),
+     *          @SWG\Property(property="created_at", type="string"),
+     *          @SWG\Property(property="updated_at", type="string"),
+     *    ),
+     *    @SWG\Property(property="client", type="object",
+     *           @SWG\Property(property="id", type="string"),
+     *           @SWG\Property(property="name", type="string"),
+     *           @SWG\Property(property="last_name", type="string"),
+     *           @SWG\Property(property="email", type="string"),
+     *           @SWG\Property(property="phone", type="string")
      *   ),
      * )
      */
     public function getLocationByCode(string $code)
     {
-        return $this->locationsRepository->getAllIncludingClientByCode($code);
+        $location = $this->locationsRepository->getAllIncludingClientByCode($code);
+
+        return [
+                'client' => $location->client,
+                'products' => $location->allProducts,
+                'location' => array_except($location, ['allProducts','client'])
+             ];
     }
 
     /**
