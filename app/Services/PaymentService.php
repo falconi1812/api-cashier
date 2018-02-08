ab0fb6016e5e65713d94fccb75c3a9168c021fcf
Christian Falcon <thefalcon1812@hotmail.com>
2018-02-08 21:22:37 +0000

    :heavy_plus_sign: :construction: Add pdf and seeders

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -81,6 +81,16 @@ class PaymentService extends Service
      *          @SWG\Property(property="quantity", type="integer"),
      *          @SWG\Property(property="created_at", type="string"),
      *          @SWG\Property(property="updated_at", type="string"),
+     *          @SWG\Property(property="products", type="object",
+     *              @SWG\Property(property="id", type="string"),
+     *              @SWG\Property(property="icon_name", type="string"),
+     *              @SWG\Property(property="icon_id", type="integer"),
+     *              @SWG\Property(property="price", type="integer"),
+     *              @SWG\Property(property="products_in_list", type="integer"),
+     *              @SWG\Property(property="products_in_payment", type="integer"),
+     *              @SWG\Property(property="created_at", type="string"),
+     *              @SWG\Property(property="updated_at", type="string")
+     *        ),
      *      )
      *    ),
      * )
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,57 +14,69 @@ class ProductsTableSeeder extends Seeder
       DB::table('products')->insert([
         [
           'id' => 1,
-          'name' => 'Adulte',
-          'price' => 50,
-          'icon_id' => 10
+          'name' => 'Adulte Smart',
+          'price' => 60,
+          'icon_id' => 728
         ],
         [
           'id' => 2,
-          'name' => 'Enfant',
-          'price' => 30,
-          'icon_id' => 10
+          'name' => 'Adulte Deluxe',
+          'price' => 85,
+          'icon_id' => 733
         ],
         [
           'id' => 3,
-          'name' => 'Billes-Adultes',
-          'price' => 30,
-          'icon_id' => 10
+          'name' => 'Enfant',
+          'price' => 35,
+          'icon_id' => 154
         ],
         [
           'id' => 4,
-          'name' => 'Billes-Enfants',
-          'price' => 20,
-          'icon_id' => 10
+          'name' => 'Arcs',
+          'price' => 50,
+          'icon_id' => 422
         ],
         [
           'id' => 5,
-          'name' => 'Costume-lapin',
-          'price' => 40,
-          'icon_id' => 10
+          'name' => 'Carton Supplémentaire',
+          'price' => 80,
+          'icon_id' => 215
         ],
         [
           'id' => 6,
-          'name' => 'Combie-Jetable',
-          'price' => 8,
-          'icon_id' => 10
+          'name' => 'Sachet Supplémentaire Adulte',
+          'price' => 30,
+          'icon_id' => 156
         ],
         [
           'id' => 7,
-          'name' => 'Boisson',
-          'price' => 2,
-          'icon_id' => 10
+          'name' => 'Sachet Supplémentaire Enfants',
+          'price' => 20,
+          'icon_id' => 159
         ],
         [
           'id' => 8,
-          'name' => 'Mangé',
-          'price' => 3,
-          'icon_id' => 10
+          'name' => 'Costume',
+          'price' => 40,
+          'icon_id' => 734
         ],
         [
           'id' => 9,
-          'name' => 'Autre',
-          'price' => 0,
-          'icon_id' => 10
+          'name' => 'Combinaison Jetable',
+          'price' => 8,
+          'icon_id' => 732
+        ],
+        [
+          'id' => 10,
+          'name' => 'Bouchons De Canon',
+          'price' => 5,
+          'icon_id' => 69
+        ],
+        [
+          'id' => 11,
+          'name' => 'Pin Protection',
+          'price' => 3,
+          'icon_id' => 638
         ]
       ]);
     }
