1f2b4027fc9f854bf25e94580d77dc4756fe5d6f
Christian Falcon <thefalcon1812@hotmail.com>
2018-03-11 11:07:46 +0000

    test

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -112,6 +112,7 @@ class LocationService extends Service
         Mail::send('welcome', $user, function ($message) use ($user, $attachment) {
             $userEmail = env('TO_TEST_EMAIL', $user['email']);
             echo $userEmail;
+            echo $user['email'];
             $message
                 ->subject(env('COMPLETE_SELL_SUBJECT', 'PaintBall arena'))
                 ->to($userEmail, $user['name'])
