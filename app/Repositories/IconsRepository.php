471d280c0d1a2da48b3e342caf4dc9417a16a9b1
Christian Falcon <thefalcon1812@hotmail.com>
2018-03-27 21:21:35 +0100

    :heavy_plus_sign: :memo: Add docs into repos

--- app/Repositories/IconsRepository.php
+++ app/Repositories/IconsRepository.php
@@ -4,15 +4,29 @@ namespace App\Repositories;
 
 use App\Icons;
 
+/**
+ * Class IconsRepository
+ * @package App\Repositories
+ */
 class IconsRepository extends Repository {
 
+    /**
+     * @var Icons
+     */
     private $icon;
 
+    /**
+     * IconsRepository constructor.
+     * @param Icons $icon
+     */
     public function __construct(Icons $icon)
     {
         $this->icon = $icon;
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function getAll()
     {
         return Icons::all();
--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -4,10 +4,20 @@ namespace App\Repositories;
 
 use Validator;
 
+/**
+ * Class Repository
+ * @package App\Repositories
+ */
 class Repository {
 
+    /**
+     * @var Validator
+     */
     public $validator;
 
+    /**
+     * Repository constructor.
+     */
     public function __construct()
     {
         $this->validator = new Validator;
